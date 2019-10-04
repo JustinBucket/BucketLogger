a763823fa0ce3fc6abf2c3d904c00dfba494310f
Justin Bucket <drdth.jdm@hotmail.com>
2019-10-03 22:41:13 -0400

    Add project files.

---
+++ BucketLogger/LogEntry.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BucketLogger
+{
+    public enum LogLevel
+    {
+        Trace,
+        Info,
+        Warning,
+        Error,
+        Critical
+    }
+    public class LogEntry
+    {
+        private DateTime LogTime { get; set; }
+        
+        public Dictionary<string, string> LogMessage { get; set; }
+        
+        public LogLevel MessageLevel { get; set; }
+
+        public string LogLine { get; set; }
+
+        public LogEntry()
+        {
+            LogMessage = new Dictionary<string, string>();
+        }
+
+        public LogEntry(LogLevel messageLevel) : this()
+        {
+            MessageLevel = messageLevel;
+        }
+
+        public LogEntry(LogLevel messageLevel, Dictionary<string, string> logMessage) : this(messageLevel)
+        {
+            LogMessage = logMessage;
+        }
+
+        public void GenerateLogLine()
+        {
+            var jsonMessage = JsonConvert.SerializeObject(LogMessage, Formatting.None);
+            LogLine = $"{MessageLevel}, {LogTime}, {jsonMessage}";
+        }
+    }
+}
---
+++ BucketLogger/Logger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BucketLogger
+{
+    public class Logger
+    {
+        public string LogName { get; set; }
+
+        private string LogPath { get; set; }
+
+        private DateTime LogDate { get; set; }
+
+        private string LogFolder { get; set; }
+
+        public Logger(string logName)
+        {
+            LogName = LogName;
+            GenerateLogFolder();
+            LogDate = DateTime.Now;
+            LogPath = Path.Combine(LogFolder,$"{LogDate.ToString("ddMMyyyy")} - Log.blf");
+            GenerateLogFile();
+        }
+
+        private void GenerateLogFolder()
+        {
+            var runFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var logFolderPath = Path.Combine(runFolder, "Logs");
+
+            if (!Directory.Exists(logFolderPath))
+                Directory.CreateDirectory(Path.Combine(runFolder, "Logs"));
+
+            LogFolder = logFolderPath;
+        }
+        
+        private void GenerateLogFile()
+        {
+            if (!File.Exists(LogPath))
+                File.Create(LogPath);
+        }
+
+        public void WriteLogLine(LogEntry logMessage)
+        {
+            using (var sw = File.AppendText(LogPath))
+            {
+                sw.WriteLine(logMessage.LogLine);
+            }
+        }
+    }
+}
